0f53c321dad063a1da7fd26e7d5c0516c6e43c2b
Sunnamsriram <59051820+sunnamsriram1@users.noreply.github.com>
2023-11-27 03:05:51 -0500

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Update Script for InstaIDUEn5 v2.1-Stable
+# Script created by @Sunnam01ram
+
+
+dependencies() {
+
+command -v git > /dev/null 2>&1 || { echo >&2 "Package GIT is Not installed ... Unable to Update ..."; exit 1; }
+
+}
+
+script() {
+
+clear
+printf "\n \e[1;92mUpdating \e[1;94mInstUser5\e[1;92m ...\n\n"
+sleep 1.5
+cd ..
+rm -rf InstaUser5
+git clone https://github.com/sunnamsriram1/InstaUser5.git
+cd InstaUser5
+chmod +x InstaUserD.py
+printf "\n\e[1;92mRestarting ...\n\e[0m"
+python3 InstaUserD.py
+cd ..
+
+}
+
+dependencies
+script
